c7da83f672c518fccc6a9e64b39c77783605f23c
Sebastian <31518719+sbra4670@users.noreply.github.com>
2018-01-23 12:38:15 -0700

    Runner successfully compiles and does absolutley nothing!

---
+++ FirstCPPP/Controller/Runner.cpp
@@ -0,0 +1,12 @@
+//
+//  Runner.cpp
+//  FirstCPPP
+//
+//  Created by Bravo, Sebastian on 1/23/18.
+//  Copyright © 2018 Bravo, Sebastian. All rights reserved.
+//
+
+int main()
+{
+    return 0;
+}
